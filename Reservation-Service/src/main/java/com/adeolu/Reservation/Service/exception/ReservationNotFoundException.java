52df2620ced527a20f404a746b78b7c44c6c36ce
Adeolu.Odunuyi <odunuyiadeolu@gmail.com>
2023-08-27 20:10:23 +0100

    Added a minor change to Reservation custom exception

--- Reservation-Service/src/main/java/com/adeolu/Reservation/Service/exception/ReservationNotFoundException.java
+++ Reservation-Service/src/main/java/com/adeolu/Reservation/Service/exception/ReservationNotFoundException.java
@@ -1,6 +1,8 @@
 package com.adeolu.Reservation.Service.exception;
 
 public class ReservationNotFoundException extends RuntimeException{
+
+    //custom exception
     public ReservationNotFoundException(String message){
         super(message);
     }
